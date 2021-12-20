f395e0e81274ed40c2c09f7445c70062a2e0e501
Util <alexxis.lg@gmail.com>
2021-12-20 10:49:21 +0100

    2

--- Utils/Sysconf.py
+++ Utils/Sysconf.py
@@ -24,12 +24,18 @@ FULL_SIZE = [min((monitor.x for monitor in MONITORS)),
              min((monitor.y for monitor in MONITORS)),
              max((monitor.x + monitor.width for monitor in MONITORS)),
              max((monitor.y + monitor.height for monitor in MONITORS))]
+is_4k = False
+for monitor in MONITORS:
+    if not monitor.is_primary:
+        continue
+    is_4k = monitor.width == 3840 and monitor.height == 2160
 ratio = 1
-if len(SCREENS) == 2:
+if is_4k:
     ratio = 1.5
 start_offset = -11
 SCREENS["hide"] = monitor_1080p((start_offset + FULL_SIZE[2] - 1920) / ratio, (FULL_SIZE[3] + 100) / ratio)
 SCREENS["semi_hide_move"] = monitor_1080p((start_offset + FULL_SIZE[2] - 1920) / ratio, (FULL_SIZE[3] - 300) / ratio)
 SCREENS["semi_hide"] = monitor_1080p((start_offset + FULL_SIZE[2] - 1920) / ratio, (FULL_SIZE[3] - 100) / ratio)
-print(FULL_SIZE)
-print(SCREENS)
+# print(MONITORS)
+# print(FULL_SIZE)
+# print(SCREENS)
