eaadc5250a85aa0735e170a1293563db663ce600
Util <alexxis.lg@gmail.com>
2021-12-24 13:39:06 +0100

    AlienWorlds 6

--- Utils/Seleniums/WaxSel.py
+++ Utils/Seleniums/WaxSel.py
@@ -36,7 +36,7 @@ def check_wax_approve(browser):
         for i in range(len(browser))[::-1]:
             browser.goto(i, False)
             approve_xpath = "/html/body/div/div/section/div[2]/div/div[5]/button"
-            start_while, start_refresh = now(), now()
+            start_while = now()
             while browser.driver.get_window_size()["width"] < 1000:
                 approve_text = browser.get_text(approve_xpath)
                 browser.print("approve_text", approve_text)
@@ -48,22 +48,19 @@ def check_wax_approve(browser):
                 if elapsed_seconds(start_while) > 60:
                     browser.goto_work()
                     return False
-                elif elapsed_seconds(start_refresh) > 15:
-                    login_button_xpath_1 = "/html/body/div/div/section/div[2]/div/div/button"
-                    login_button_xpath_2 = "/html/body/div/div/div/div/div/div[3]/button"
-                    login_button_xpath_3 = "/html/body/div/div/div/div/div[5]/div/div/div/div[4]/button"
-                    login_buttons_xpath = [login_button_xpath_1, login_button_xpath_2, login_button_xpath_3]
+                login_button_xpath_1 = "/html/body/div/div/section/div[2]/div/div/button"
+                login_button_xpath_2 = "/html/body/div/div/div/div/div/div[3]/button"
+                login_button_xpath_3 = "/html/body/div/div/div/div/div[5]/div/div/div/div[4]/button"
+                login_buttons_xpath = [login_button_xpath_1, login_button_xpath_2, login_button_xpath_3]
+                login_button = browser.get_element(login_buttons_xpath)
+                while login_button and browser.driver.get_window_size()["width"] < 1000:
+                    login_button_text = get_element_text(login_button)
+                    print("login_button_text", login_button_text)
                     login_button = browser.get_element(login_buttons_xpath)
-                    while login_button and browser.driver.get_window_size()["width"] < 1000:
-                        login_button_text = get_element_text(login_button)
-                        print("login_button_text", login_button_text)
-                        login_button = browser.get_element(login_buttons_xpath)
-                        msg = browser.name + "wax approve have to login"
-                        say(msg)
-                        telegram_msg(msg)
-                        sleep(10)
-                    browser.refresh()
-                    start_refresh = now()
+                    msg = browser.name + "wax approve have to login"
+                    say(msg)
+                    telegram_msg(msg)
+                    sleep(10)
                 if not browser.goto(i, False):
                     del browser.windows_url[i]
                     browser.goto_work()
