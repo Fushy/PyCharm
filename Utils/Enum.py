ee520ccea1f02047e9453f85589ad67bc322c91a
Alexis <alexxis.lg@gmail.com>
2022-05-27 17:12:40 +0200

    AlienWorlds

--- Utils/Enum.py
+++
@@ -1,7 +0,0 @@
-ON: int = 1
-OFF: int = 0
-OUT: int = -1
-LAST: int = -1
-FIRST: int = 0
-
-LAUNCH: str = "LAUNCH"
--- Utils/Paths.py
+++
@@ -1,5 +0,0 @@
-# if not os.path.exists(pathname):
-#     Path(pathname).mkdir(parents=True, exist_ok=True)
-import sys
-
-print(sys.path)
